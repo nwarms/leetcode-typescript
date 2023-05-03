856bd39105e54ca4a5e509418ccbe8f586c8f09a
Nick Warms <nick.warms@skybettingandgaming.com>
2023-05-03 15:09:51 +0100

    Fix test runner

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['<rootDir>/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['node_modules', 'src/database', 'src/test', 'src/types'],
+  reporters: ['default', 'jest-junit'],
+  globals: { 'ts-jest': { diagnostics: false } },
+  transform: {},
 };
